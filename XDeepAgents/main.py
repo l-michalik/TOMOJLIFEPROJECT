b7d37e5644da4cc0f1d9c7c7020bdec790234117
Łukasz Michalik <l.michalik@mdbootstrap.com>
2026-03-25 16:35:25 +0100

    work

--- XDeepAgents/main.py
+++ XDeepAgents/main.py
@@ -1,20 +1,58 @@
 from deepagents import create_deep_agent
 from dotenv import load_dotenv
+import os
+from typing import Literal
+from tavily import TavilyClient
 
 load_dotenv()
 
-def get_weather(city: str) -> str:
-    """A mock function to get the weather for a city. In a real implementation, this would call an API."""
-    return f"It's always sunny in {city}!"
+tavily_client = TavilyClient()
+
+def internet_search(
+    query: str,
+    max_results: int = 5,
+    topic: Literal["general", "news", "finance"] = "general",
+    include_raw_content: bool = False,
+):
+    """
+    Run a web search using TavilyClient.
+
+    Args:
+        query (str): The search query.
+        max_results (int): Maximum number of results to return. Default is 5.
+        topic (Literal): The topic of the search. Default is "general".
+        include_raw_content (bool): Whether to include raw content in results. Default is False.
+
+    Returns:
+        dict: Search results.
+    """
+    return tavily_client.search(
+        query=query,
+        max_results=max_results,
+        include_raw_content=include_raw_content,
+        topic=topic,
+    )
+
+RESEARCH_INSTRUCTIONS = """You are an expert researcher. Your job is to conduct thorough research and then write a polished report.
+
+You have access to an internet search tool as your primary means of gathering information.
+
+## `internet_search`
+
+Use this to run an internet search for a given query. You can specify the max number of results to return, the topic, and whether raw content should be included.
+"""
 
 agent = create_deep_agent(
-    tools=[get_weather],
-    system_prompt="You are a helpful assistant",
-    model="gpt-4.1-mini"
+    tools=[internet_search],
+    system_prompt=RESEARCH_INSTRUCTIONS,
+    model="openai:gpt-5.4",
 )
 
-response = agent.invoke(
-    {"messages": [{"role": "user", "content": "what is the weather in sf"}]}
-)
+def main():
+    user_query = "What is langgraph?"
+    result = agent.invoke({"messages": [{"role": "user", "content": user_query}]})
+    
+    print(result["messages"][-1].content)
 
-print(response["messages"][-1].content)
+if __name__ == "__main__":
+    main()

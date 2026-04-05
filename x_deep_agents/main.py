921fd909f1bd042ee08af4acc3fa163dbcb4c80e
Łukasz Michalik <l.michalik@mdbootstrap.com>
2026-04-05 09:41:16 +0200

    work

--- x_deep_agents/main.py
+++ x_deep_agents/main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from x-deep-agents!")
+from __future__ import annotations
+from deepagents import create_deep_agent
+from dotenv import load_dotenv
 
+load_dotenv()
 
-if __name__ == "__main__":
-    main()
+def get_weather(city: str) -> str:
+    """Get weather for a given city."""
+    return f"It's always sunny in {city}!"
+
+agent = create_deep_agent(
+    tools=[get_weather],
+    model="openai:gpt-5.4",
+    system_prompt="You are a helpful assistant",
+)
+
+response = agent.invoke(
+    {"messages": [{"role": "user", "content": "what is the weather in sf"}]}
+)
+
+print(response)

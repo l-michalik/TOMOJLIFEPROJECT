aa95aac34f2f76aabec76f5e567e822126ba8205
Łukasz Michalik <l.michalik@mdbootstrap.com>
2026-04-05 10:10:21 +0200

    work

---
+++ x_deep_agents/f2.py
@@ -0,0 +1,45 @@
+from dotenv import load_dotenv
+import os
+from typing import Literal
+from tavily import TavilyClient
+from deepagents import create_deep_agent
+
+load_dotenv()
+
+tavily_client = TavilyClient()
+
+def internet_search(
+    query: str,
+    max_results: int = 5,
+    topic: Literal["general", "news", "finance"] = "general",
+    include_raw_content: bool = False,
+):
+    """Run a web search"""
+    print(f"Running internet search for query: {query}")
+    return tavily_client.search(
+        query,
+        max_results=max_results,
+        include_raw_content=include_raw_content,
+        topic=topic,
+    )
+    
+# System prompt to steer the agent to be an expert researcher
+research_instructions = """You are an expert researcher. Your job is to conduct thorough research and then write a polished report.
+
+You have access to an internet search tool as your primary means of gathering information.
+
+## `internet_search`
+
+Use this to run an internet search for a given query. You can specify the max number of results to return, the topic, and whether raw content should be included.
+"""
+
+agent = create_deep_agent(
+    model="openai:gpt-4.1",
+    tools=[internet_search],
+    system_prompt=research_instructions,
+)
+
+result = agent.invoke({"messages": [{"role": "user", "content": "What is langgraph? Check the web and write a report."}]})
+
+# Print the agent's response
+print(result["messages"][-1].content)
